3248bae369016af57da44af7514764b14a0420aa
Sergio Bernal <sergiobernal909@gmail.com>
2023-10-06 00:27:10 -0600

    🔧 Chore: remove md plugin. add icons

--- lua/plugins/plain-plugin.lua
+++ lua/plugins/plain-plugin.lua
@@ -2,5 +2,4 @@ return {
   { 'frazrepo/vim-rainbow', },
   { 'sheerun/vim-polyglot' },
   { 'jiangmiao/auto-pairs', },
-  { 'plasticboy/vim-markdown', }
 }
--- lua/plugins/telescope-plugin.lua
+++ lua/plugins/telescope-plugin.lua
@@ -14,7 +14,7 @@ return {
         },
       },
       defaults = {
-        file_ignore_patterns = { "node_modules", "plugged" },
+        file_ignore_patterns = { "node_modules", "plugged", "dist" },
       },
       extensions = {
         package_info = {
