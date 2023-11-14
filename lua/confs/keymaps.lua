701f03f6d71d9223bf66666535a4a3e3e01a06b6
Sergio Bernal <sergio.bernal@telus.com>
2023-11-14 12:38:07 -0600

    refactor: remap close keymaps

--- lua/confs/keymaps.lua
+++ lua/confs/keymaps.lua
@@ -9,12 +9,12 @@ vim.keymap.set("n", "<leader>fh", ":lua require('telescope.builtin').help_tags()
 vim.keymap.set("n", "<leader>tf", ":Neotree toggle current reveal_force_cwd right<CR>", opts)
 vim.keymap.set("n", "<leader>tb", ":Neotree toggle show buffers right<CR>", opts)
 vim.keymap.set("n", "<leader>tg", ":Neotree float git_status<CR>", opts)
-vim.keymap.set("n", "<leader>x", ":bd<CR>", opts)
 vim.keymap.set("n", "<leader>w", ":w<CR>", opts)
-vim.keymap.set("n", "<leader>wq", ":wq<CR>", opts)
 vim.keymap.set("n", "<leader>wa", ":wall<CR>", opts)
-vim.keymap.set("n", "<leader>q", ":q<CR>", opts)
+vim.keymap.set("n", "<leader>wq", ":wq<CR>", opts)
 vim.keymap.set("n", "<leader>qa", ":qall<CR>", opts)
+vim.keymap.set("n", "<leader>qq", ":q<CR>", opts)
+vim.keymap.set("n", "<leader>qx", ":bd<CR>", opts)
 
 vim.cmd([[
   nmap <Leader>z 1z=
@@ -58,3 +58,10 @@ end, { desc = "Next todo comment" })
 vim.keymap.set("n", "[t", function()
   require("todo-comments").jump_prev()
 end, { desc = "Previous todo comment" })
+
+-- folke/trouble
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
