3c0674f94b79bfdb4600e91c2fbd9413211ea66f
Sergio Bernal <sergio.bernal@telus.com>
2023-11-24 10:37:55 -0600

    fix: remove lazy for indent

--- lua/plugins/simple-indent-plugin.lua
+++ lua/plugins/simple-indent-plugin.lua
@@ -1,6 +1,5 @@
 return {
   'lucastavaresa/simpleIndentGuides.nvim',
-  event = "VeryLazy",
   config = function()
     vim.opt.list = true -- enable in all buffers
     require("simpleIndentGuides").setup("│", "·")
